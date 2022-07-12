525d87ba66fa3674585ae8af5a36322f03b80b14
cau-choihan <mostone97@gmail.com>
2022-07-12 18:12:54 +0900

    FIX(Bubble sort) 초기형태 버블정렬로 수정

--- Sort algorithm 정렬 알고리즘/Bubble sort 버블 정렬/Bubble sort 버블 정렬.js
+++ Sort algorithm 정렬 알고리즘/Bubble sort 버블 정렬/Bubble sort 버블 정렬.js
@@ -7,17 +7,18 @@ console.log(arr); //=>[100,99,98 ... 3, 2, 1]
 
 //bubble sort method
 function bubbleSort(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[i] > arr[j]) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = arr.length - 1; j > i; j--) {
+            if (arr[j - 1] > arr[j]) {
                 let temp;
-                temp = arr[i];
-                arr[i] = arr[j];
+                //j와 j-1 인덱스를 가진 값을 교환하기 위해 temp를 선언한다
+                //보기 편하게 이곳에 선언했지만 method 상단에 temp를 선언하는게 좋다.
+                temp = arr[j - 1];
+                arr[j - 1] = arr[j];
                 arr[j] = temp;
             }
         }
     }
-    return arr;
 }
 
 bubbleSort(arr);
